424c14ee2faf5142fbb0dd7c2b1429d4473f6525
rohan-cli <rohn.mishra.7@gmail.com>
2025-07-21 00:19:37 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   emailjs: {
-    serviceId: 'service_pb6zhnr',
-    templateId: 'template_cdo74hf', 
-    publicKey: '2euv2r7Nr_lF9Nn7j'
+    serviceId: '',
+    templateId: '', 
+    publicKey: ''
   }
 };
